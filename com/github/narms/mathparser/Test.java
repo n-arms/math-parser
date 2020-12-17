9bd32183f0aa33a63e0d33322a7c58bb1e32f9bc
n-arms <armstrong@Armstrongs-MacBook.local>
2020-12-17 16:37:07 -0500

    Populate Token.java
    * TokenType enum
    * Constructor
    * Getters and Setters for token's type and value

--- com/github/narms/mathparser/Test.java
+++ com/github/narms/mathparser/Test.java
@@ -2,6 +2,8 @@ package com.github.narms.mathparser;
 
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        String s = "abcde";
+        s = s+"fghi";
+        System.out.println(s);
     }
 }
--- com/github/narms/mathparser/Token.java
+++ com/github/narms/mathparser/Token.java
@@ -1,5 +1,45 @@
 package com.github.narms.mathparser;
 
+enum TokenType{
+    NUM, SYM, OP;
+}
+
 public class Token {
-    
+    private TokenType type;
+    private String value;
+
+    public Token(){
+        this.value = "";
+    }
+
+    public void addChar(char c){
+        this.value+=c;
+    }
+
+    public void setType(){
+        try{
+            Double.parseDouble(this.value);
+            this.type = TokenType.NUM;
+        }finally{
+            switch(this.value.charAt(0)){
+                case '+':
+                this.type = TokenType.OP;
+                break;
+                case '-':
+                this.type = TokenType.OP;
+                break;
+                default:
+                this.type = TokenType.SYM;
+                break;
+            }
+        }
+    }
+
+    public TokenType getType(){
+        return this.type;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
 }
