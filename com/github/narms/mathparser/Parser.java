88f9f93350346c63d3f16af1b2be9c60c69ec04c
n-arms <armstrong@Armstrongs-MacBook.local>
2020-12-17 20:56:56 -0500

    Populate Parser.java with Parser.parseTerm()
    * Can parse any + or - in an ArrayList<Syntax>

--- com/github/narms/mathparser/Parser.java
+++ com/github/narms/mathparser/Parser.java
@@ -1,5 +1,26 @@
 package com.github.narms.mathparser;
 
+import java.util.ArrayList;
+
+import com.github.narms.mathparser.expressions.BinOp;
+
 public class Parser {
-    
+    public static ArrayList<Syntax> parseTerm(ArrayList<Syntax> structure){
+        int i = 0;
+        ArrayList<Syntax> output = structure;
+        while (i<output.size()){
+            if (output.get(i) instanceof Token){
+                if (((Token) output.get(i)).getValue().equals("+") || ((Token) output.get(i)).getValue().equals("-")){
+                    output.set(i, new BinOp(((Token)output.get(i)).getValue(), 
+                    ((ExpressionSyntax)output.get(i-1)), 
+                    ((ExpressionSyntax)output.get(i+1))));
+                    output.remove(i+1);
+                    output.remove(i-1);
+
+                }
+                else{i++;}
+            }else{i++;}
+        }
+        return output;
+    }
 }
--- com/github/narms/mathparser/Test.java
+++ com/github/narms/mathparser/Test.java
@@ -1,10 +1,24 @@
 package com.github.narms.mathparser;
+
+import java.util.ArrayList;
+
 import com.github.narms.mathparser.expressions.*;
 
 public class Test{
     public static void main(String[] args){
-        ExpressionSyntax tree = new BinOp("+", new Const(5), new BinOp("-", new Const(10), new Const(3)));
-        System.out.println(tree);
-        System.out.println(tree.eval());
+        ArrayList<Syntax> struct = new ArrayList<Syntax>();
+        Token t = new Token();
+        t.addChar('+');
+        t.setType();
+        struct.add(new Const(5));
+        struct.add(t);
+        struct.add(new Const(3));
+        Token t2 = new Token();
+        t2.addChar('-');
+        t2.setType();
+        struct.add(t2);
+        struct.add(new Const(2));
+        System.out.println(Parser.parseTerm(struct).get(0));
+        System.out.println(((ExpressionSyntax)Parser.parseTerm(struct).get(0)).eval());
     }
 }
